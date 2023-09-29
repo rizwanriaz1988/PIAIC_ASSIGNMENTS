5fc2a06d2030f794f9867127aed450d807c31709
rizwan <rizwanriaz1988@gmail.com>
2023-09-29 20:58:11 +0500

    bank project completed

---
+++ Quarter 01/project11_oop_mybank/README.md
@@ -0,0 +1,4 @@
+# Learning Object Oriented Programming with TypeScript MyBank Console App
+In this project you are going to follow this video which explains object oriented programming using C# and write the same code in TypeScript.
+
+Create a GitHub repository for the project and submit its URL in the project submission form.
---
+++ Quarter 01/project11_oop_mybank/index.ts
@@ -0,0 +1,197 @@
+import inquirer from "inquirer";
+import chalk from "chalk"
+import chalkAnimation from "chalk-animation"
+
+const mainQuestions = [
+    {
+        type:"list",
+        name:"questions",
+        message:"Select Operation",
+        choices:["Add Customer","Make Transaction","View Balance","Exit"]
+    }
+]
+const addCustomerQuestions = [
+    {
+        type:"input",
+        name:"custFirstName",
+        message:"Enter First Name:"        
+    },
+    {
+        type:"input",
+        name:"custLastName",
+        message:"Enter Last Name:"        
+    },
+    {
+        type:"list",
+        name:"custGender",
+        message:"Select Gender:",
+        choices:["Male","Female","Transgender"]        
+    },
+    {
+        type:"input",
+        name:"custAge",
+        message:"Enter Age:",
+        validate: validNumber        
+    },
+    {
+        type:"input",
+        name:"custMobile",
+        message:"Enter Mobile Number:"        
+    },
+    {
+        type:"number",
+        name:"custBalance",
+        message:"Enter Initial Balance:",
+        validate: validNumber        
+    },
+]
+
+const againAskQuestions = [
+    {
+        type:"confirm",
+        name:"askAgain",
+        message:"Are you want to run again?"
+    }
+]
+function validNumber(input:any){
+    if(input){
+        if(isNaN(input)){
+            return "Enter Valid Age"
+        }else{
+            return true
+        }
+    }else{
+        return "Enter Some Value"
+    }
+}
+async function againAsking() {
+    const response = await inquirer.prompt(againAskQuestions)
+    return response.askAgain
+    
+}
+
+class customerDetail {
+
+    custFirst:string;
+    custLast:string;
+    custGender:string;
+    custAge:number;
+    custMobile:string;
+    custAccountNum:any;
+    custBalance:number;
+
+    constructor(_custFirst:string,
+                _custLast:string,
+                _custGender:string,
+                _custAge:number,
+                _custMobile:string,
+                _custBalance:number,){
+
+        this.custFirst = _custFirst
+        this.custLast = _custLast
+        this.custGender = _custGender
+        this.custAge = _custAge
+        this.custMobile = _custMobile
+        this.custAccountNum = this.accountNumber()
+        this.custBalance = _custBalance
+    }
+    accountNumber(){
+        // return Math.floor(Math.random()*899999999999999) + 100000000000000 
+        return Math.floor(Math.random()*899) + 100 
+    }
+    
+}
+
+const customerList:any =[]
+
+async function questioning() {
+    await inquirer.prompt(mainQuestions)
+    .then(async(ansu)=>{
+        if(ansu.questions === "Add Customer"){
+            await addCustomer()
+        }else if(ansu.questions === "Make Transaction"){
+            await custTransaction()
+        }else if(ansu.questions === "View Balance"){
+            await customerBalance()
+        }else if(ansu.questions === "Exit"){
+            process.exit()
+        }
+    })
+}
+
+async function addCustomer() {
+    await inquirer.prompt(addCustomerQuestions)
+    .then((ans)=>{
+        
+        
+        const customer = new customerDetail(ans.custFirstName,
+                                            ans.custLastName,
+                                            ans.custGender,
+                                            ans.custAge,
+                                            ans.custMobile,
+                                            ans.custBalance,)
+        customerList.push(customer)
+        console.log(customerList);
+    })
+}
+
+async function custTransaction() {
+    await inquirer.prompt([
+        {
+            type: "input",
+            name:"custAccNum",
+            message:"Enter Customer AC/No"
+        }
+    ]).then(async(ac)=>{
+        const accountNum = customerList.find((x:any)=>x.custAccountNum === parseInt(ac.custAccNum))
+        if(accountNum){
+            await inquirer.prompt([
+                {
+                    type: "list",
+                    name:"selectMode",
+                    message:"Select Transaction Type",
+                    choices:["Debit","Credit"]
+                },
+                {
+                    type: "number",
+                    name:"transactionAmount",
+                    message:"Enter Amount",
+
+                }
+            ]).then((mode)=>{
+                if(mode.selectMode === "Debit"){
+                    accountNum.custBalance = accountNum.custBalance - parseInt(mode.transactionAmount)
+                }else if(mode.selectMode === "Credit"){
+                    accountNum.custBalance = accountNum.custBalance + parseInt(mode.transactionAmount)
+                }
+            })
+        }else{
+            return "Enter Correct Account Number"
+        }
+    })
+}
+
+async function customerBalance() {
+    await inquirer.prompt([
+        {
+            type: "input",
+            name:"custAccNum",
+            message:"Enter Customer AC/No"
+        }
+    ]).then(async(ac)=>{
+        const accountNum = customerList.find((x:any)=>x.custAccountNum === parseInt(ac.custAccNum))
+        if(accountNum){
+            console.log(accountNum.custBalance);
+        }
+    })
+}
+
+async function main() {
+    do{
+        await questioning()
+    }while(await againAsking())
+    
+}
+
+main()
+
