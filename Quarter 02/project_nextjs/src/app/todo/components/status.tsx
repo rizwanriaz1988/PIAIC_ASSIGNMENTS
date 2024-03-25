d7d63ee48f6b13779718559f9ef8bcce7475fe1d
rizwanriaz1988 <rizwanriaz1988@gmail.com>
2024-03-25 19:32:51 +0500

    working is pending

--- Quarter 02/project_nextjs/src/app/todo/components/status.tsx
+++ Quarter 02/project_nextjs/src/app/todo/components/status.tsx
@@ -167,151 +167,146 @@ const TodoList = (props: todolist_type) => {
 // =================== return ====================== return ==================== return ==================
   return (
     <div>
-      {isLoading ? <Loading2 /> : 
+    {isLoading ? (
+      <Loading2 />
+    ) : (
       <div>
         {Array.isArray(todos) &&
-        todos.length > 0 &&
         props.todoList === "pending" ? (
-          todos.map(
-            (todo) =>
-              !todo.status && (
-                
-                <div key={todo.id} className="py-2">
-                  <Suspense>
-                                      
-                  </Suspense>
-                  {/* ======================================show of edit inline */}
-                  {btnState && btnId === todo.id && formVisible ? (
-                    <Newtask
-                      btnS={() => setBtnSave(true)}
-                      btnState={btnState}
-                      editData={editData}
-                      onCancel={() => {
-                        setFormVisible(false);
-                      }}
-                    />
-                  ) : (
-                    //use effect for showing see more button
-                    // =======================================================================================
-                    <div className="bg-slate-800 rounded-lg mx-2">
-                      <div className="p-1 ">
-                        <h3 className="break-words bg-slate-950 rounded-md px-5 py-1 text-2xl border-slate-500 border">{todo.title}</h3>
-
-                        <p
-                          ref={(el) => (descriptionRef.current[todo.id] = el!)}
-                          className={`break-words ${
-                            moreButtons[todo.id] ? "" : "line-clamp-2"
-                          }  rounded-md px-5 my-1`}
-                        >
-                          {todo.description}
-                        </p>
-
-                        {overflowStatus[todo.id] && (
-                          <div className=" flex justify-end">
-                            <button
-                              className="text-xs mx-2  text-blue-400 hover:text-blue-500 "
-                              onClick={() => activeSeemore(todo.id)}
+          todos.length > 0 ? (
+            todos.map(
+              (todo) =>
+                !todo.status && (
+                  <div key={todo.id} className="py-2">
+                    {/* ======================================show of edit inline */}
+                    {btnState && btnId === todo.id && formVisible ? (
+                      <Newtask
+                        btnS={() => setBtnSave(true)}
+                        btnState={btnState}
+                        editData={editData}
+                        onCancel={() => {
+                          setFormVisible(false);
+                        }}
+                      />
+                    ) : (
+                      //use effect for showing see more button
+                                              // =======================================================================================
+                        <div className="bg-slate-800 rounded-lg mx-2">
+                          <div className="p-1 ">
+                            <h3 className="break-words bg-slate-950 rounded-md px-5 py-1 text-2xl border-slate-500 border">{todo.title}</h3>
+                            <p
+                              ref={(el) => (descriptionRef.current[todo.id] = el!)}
+                              className={`break-words ${
+                                moreButtons[todo.id] ? "" : "line-clamp-2"
+                              }  rounded-md px-5 my-1`}
                             >
-                              {moreButtons[todo.id] ? "see less" : "see more"}
+                              {todo.description}
+                            </p>
+                            {overflowStatus[todo.id] && (
+                              <div className=" flex justify-end">
+                                <button
+                                  className="text-xs mx-2  text-blue-400 hover:text-blue-500 "
+                                  onClick={() => activeSeemore(todo.id)}
+                                >
+                                  {moreButtons[todo.id] ? "see less" : "see more"}
+                                </button>
+                              </div>
+                            )}
+                          </div>
+
+                          <div className="flex  justify-end mx-2">
+                            <div className="flex items-center ml-5  py-1">
+                              <button onClick={() => deleteTask(todo.id)} title="Click to delete">
+                                <RiDeleteBinLine className="h-5 w-5  text-red-500 hover:text-red-600 "   />
+                              </button>
+                              <button onClick={() => editTask(todo.id)} title="Click to edit">
+                                <FiEdit className="h-5 w-5 mx-2  text-red-500 hover:text-red-600" />
+                              </button>
+                            </div>
+                            <button
+                              onClick={() => todostatus(todo.id, true)}
+                              title="Click to complete"
+                            >
+                              <MdDoneOutline className="h-5 w-5 text-green-500 hover:text-green-700"/>
                             </button>
                           </div>
-                        )}
-                        
-                      </div>
-
-                      <div className="flex  justify-end mx-2">
-                        <div className="flex items-center ml-5  py-1">
-                          <button onClick={() => deleteTask(todo.id)} title="Click to delete">
-                            <RiDeleteBinLine className="h-5 w-5  text-red-500 hover:text-red-600 "   />
-                          </button>
-                          <button onClick={() => editTask(todo.id)} title="Click to edit">
-                            <FiEdit className="h-5 w-5 mx-2  text-red-500 hover:text-red-600" />
-                          </button>
                         </div>
-                        <button
-                          
-                          onClick={() => todostatus(todo.id, true)}
-                          // className="bg-green-700 hover:bg-green-700 m-1 text-black hover:text-white rounded-md"
-                          title="Click to complete">
-                          {/* Completed */}
-                          <MdDoneOutline className="h-5 w-5 text-green-500 hover:text-green-700"/>
-                        </button>
-                      </div>
+                      )}
                     </div>
-                  )}
-                </div>
+                  )
               )
-          )
-        ) : Array.isArray(todos) &&
-          todos.length > 0 &&
-          props.todoList === "completed" ? (
-          todos.map(
-            (todo) =>
-              todo.status && (
-                <div key={todo.id} className="py-2">
-                  {/* ======================================show of edit inline */}
+            ) : (
+              <div className="text-lg text-red-500 pl-2">No Tasks Found</div>
+            )
+          ) : (
+            Array.isArray(todos) &&
+            props.todoList === "completed" ? (
+              todos.length > 0 ? (
+                todos.map(
+                  (todo) =>
+                    todo.status && (
+                      <div key={todo.id} className="py-2">
+                        {/* ======================================show of edit inline */}
+                        {btnState && btnId === todo.id && formVisible ? (
+                          <Newtask
+                            btnS={() => setBtnSave(true)}
+                            btnState={btnState}
+                            editData={editData}
+                            onCancel={() => {
+                              setFormVisible(false);
+                            }}
+                          />
+                        ) : (
+                          <div className="bg-slate-800 rounded-lg mx-2">
+                            <div className="p-1 ">
+                              <h3 className="break-words bg-slate-950 rounded-md px-5 py-1 text-2xl border-slate-500 border">{todo.title}</h3>
+                              <p
+                                ref={(el) => (descriptionRef.current[todo.id] = el!)}
+                                className={`break-words ${
+                                  moreButtons[todo.id] ? "" : "line-clamp-2"
+                                }  rounded-md px-5 my-1`}
+                              >
+                                {todo.description}
+                              </p>
+                              {overflowStatus[todo.id] && (
+                                <div className=" flex justify-end">
+                                  <button
+                                    className="text-xs mx-2  text-blue-400 hover:text-blue-500 "
+                                    onClick={() => activeSeemore(todo.id)}
+                                  >
+                                    {moreButtons[todo.id] ? "see less" : "see more"}
+                                  </button>
+                                </div>
+                              )}
+                            </div>
 
-                  {btnState && btnId === todo.id && formVisible ? (
-                    <Newtask
-                      btnS={() => setBtnSave(true)}
-                      btnState={btnState}
-                      editData={editData}
-                      onCancel={() => {
-                        setFormVisible(false);
-                      }}
-                    />
-                  ) : (
-                    <div className="bg-slate-800 rounded-lg mx-2">
-                      <div className="p-1 ">
-                        <h3 className="break-words bg-slate-950 rounded-md px-5 py-1 text-2xl border-slate-500 border">{todo.title}</h3>
-                        <p
-                          ref={(el) => (descriptionRef.current[todo.id] = el!)}
-                          className={`break-words ${
-                            moreButtons[todo.id] ? "" : "line-clamp-2"
-                          }  rounded-md px-5 my-1`}
-                        >
-                          {todo.description}
-                        </p>
-                        {overflowStatus[todo.id] && (
-                          <div className=" flex justify-end">
-                            <button
-                              className="text-xs mx-2  text-blue-400 hover:text-blue-500 "
-                              onClick={() => activeSeemore(todo.id)}
-                            >
-                              {moreButtons[todo.id] ? "see less" : "see more"}
-                            </button>
+                            <div className="flex items-center justify-end gap-2 mx-2 ">
+                              <div className="flex items-center gap-2 py-1">
+                                <button onClick={() => deleteTask(todo.id)} title="Click to delete">
+                                  <RiDeleteBinLine className="h-5 w-5  text-red-500 hover:text-red-600" />
+                                </button>
+                                <button onClick={() => editTask(todo.id)} title="Click to edit">
+                                  <FiEdit className="h-5 w-5   text-red-500 hover:text-red-600" />
+                                </button>
+                              </div>
+                              <button
+                                onClick={() => todostatus(todo.id, false)} title="Click to undo"
+                              >
+                                <ImUndo2 className="h-5 w-5   text-red-500 hover:text-red-600"/>
+                              </button>
+                            </div>
                           </div>
                         )}
                       </div>
-
-                      <div className="flex items-center justify-end gap-2 mx-2 ">
-                        <div className="flex items-center gap-2 py-1">
-                          <button onClick={() => deleteTask(todo.id)} title="Click to delete">
-                            <RiDeleteBinLine className="h-5 w-5  text-red-500 hover:text-red-600" />
-                          </button>
-                          <button onClick={() => editTask(todo.id)} title="Click to edit">
-                            <FiEdit className="h-5 w-5   text-red-500 hover:text-red-600" />
-                          </button>
-                        </div>
-                        <button
-                          
-                          onClick={() => todostatus(todo.id, false)} title="Click to undo"
-                          // className="bg-red-700 hover:bg-red-700  text-black hover:text-white rounded-none "
-                        >
-                          <ImUndo2 className="h-5 w-5   text-red-500 hover:text-red-600"/>
-                          {/* To Do */}
-                        </button>
-                      </div>
-                    </div>
-                  )}
-                </div>
+                    )
+                )
+              ) : (
+                <div className="text-lg text-red-500 pl-2">No Tasks Found</div>
               )
-          )
-        ) : (
-          <p>No Task Exist</p>
-        )}
-      </div>
-      }
+            ) : null
+          )}
+        </div>
+      )}
     </div>
   );
 };
